d02a6c18732963274b2a45c2c5d887491ce8b6de
aswanijitheesh <69705905+aswanijitheesh@users.noreply.github.com>
2020-08-19 10:01:13 +0530

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,9 +1,9 @@
 var credentials = {
-  google: {
-    clientID: '501663784382-67629kt9ihpopnhmcdvk3honn6a1tim4.apps.googleusercontent.com',
-    clientSecret: '8744NNHYn1HEBNt6Yr7YYSK3',
-    callbackURL: 'http://127.0.0.1:3000/auth/google/callback'
-  },
+   google: {
+      clientID: 'get_google_id',
+      clientSecret: 'get_google_client_secret',
+      callbackURL: 'http://1ocalhost/auth/google/callback'
+    },
 };
 
 module.exports = credentials;
